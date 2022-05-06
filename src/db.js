92ac7cfb036ac96f2f4af63394f615395cf7735d
Saúl Olguin Alanis <35753744+DonSaul@users.noreply.github.com>
2022-05-05 23:39:28 -0500

    Login cambios

---
+++ src/db.js
@@ -0,0 +1,46 @@
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database : 'dba_lenguaje'
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+
+});
+
+
+
+//var query = 'SELECT IF ( EXISTS (SELECT * FROM dba_lenguaje.user WHERE user = ? and pass = ? ), 1, 0)'
+
+function logintest(){
+        con.query('SELECT * FROM dba_lenguaje.user', function (error, rows, fields) {
+                if (error) throw error;
+
+                    var user = document.getElementById("user").value;
+                    var password = document.getElementById("pass").value;
+                    console.log(user + password);
+                    var flag = 0;
+                    for (let i = 0; i < rows.length; i++) {
+                        console.log(i);
+                        if (user === rows[i].user && password === rows[i].pass){
+                        alert('existe');
+                        document.location.href = 'hotel.html';
+                        flag = 1;
+                        }
+                    }
+                    if (flag === 0){
+                        alert('Error: Usuario o clave invalida.')
+                    }
+
+                    
+                });
+          
+
+
+            }
