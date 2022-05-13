53cb18c51355b49e19457bccb57a3afb5c19958a
saul olguin <sauloainc@gmail.com>
2022-05-13 12:01:05 -0500

    Crud changes

--- src/crud.js
+++ src/crud.js
@@ -19,23 +19,157 @@ var con = mysql.createConnection({
   
   });
 
-  var data=[];
-  
+
+  var contador = 0;
    function tabla(){
     con.query("SELECT * FROM dba_lenguaje.user", function (err, result, fields) {
         if (err) throw err;
-       
+    
+        contador = result.length;
         Object.keys(result).forEach(function(key) {
             var row = result[key];
-            console.log(row.name)
-          });
-     
+            
+          
+         
+           
+                $('#my_table').append(`<tr>
+                    <td>${row.idUser}</td>
+                      <td>${row.user}</td>
+                      <td>${row.nombre}</td>
+                      <td>${row.pass}</td>
+                  </tr>`);
 
+               
+                
+                  
+        });
+
+       
+   
+    
+      
         
    
         });
+    
+
     }
+
+  
     tabla();
 
-    
   
+function Edicion(){
+
+    var numero =  document.getElementById('selector').value;
+    if (numero.length != 0){
+    console.log(numero);
+    con.query("SELECT * FROM dba_lenguaje.user WHERE idUser =? ",[numero], function (err, result, fields) {
+    if (err) throw err;
+   
+    Object.keys(result).forEach(function(key) {
+        var row = result[key];
+        
+      
+       var id =  row.idUser;
+       var usuario = row.user;
+       var nombre = row.nombre;
+       var contra = row.pass;
+      
+       document.getElementById("usernuevo").value = usuario;
+       document.getElementById("nombrenuevo").value = nombre;
+       document.getElementById("passnuevo").value = contra;
+           
+            
+              
+    });
+
+    });
+
+    }else{
+        alert('Ecribe un id valido');
+    }
+
+}
+function Cedicion(){
+  
+    var numero =  document.getElementById('selector').value;
+    var usuario = document.getElementById("usernuevo").value;
+    var nombre = document.getElementById("nombrenuevo").value;
+    var contra = document.getElementById("passnuevo").value;
+    con.query("UPDATE dba_lenguaje.user set user = ?,nombre =?,pass=?  WHERE idUser =? ",[usuario,nombre,contra,numero], function (err, result, fields) {
+    if (err) throw err;
+    else{
+        alert('usuario actualizado');
+        tabla();
+
+    }
+
+    });
+
+}
+    
+function Edicion(){
+
+    var numero =  document.getElementById('selector').value;
+    if (numero.length != 0){
+    console.log(numero);
+    con.query("SELECT * FROM dba_lenguaje.user WHERE idUser =? ",[numero], function (err, result, fields) {
+    if (err) throw err;
+   
+    Object.keys(result).forEach(function(key) {
+        var row = result[key];
+        
+      
+       var id =  row.idUser;
+       var usuario = row.user;
+       var nombre = row.nombre;
+       var contra = row.pass;
+      
+       document.getElementById("usernuevo").value = usuario;
+       document.getElementById("nombrenuevo").value = nombre;
+       document.getElementById("passnuevo").value = contra;
+           
+            
+              
+    });
+
+    });
+
+    }else{
+        alert('Ecribe un id valido');
+    }
+
+}
+function borrar(){
+    var numero =  document.getElementById('selector').value;
+  
+    con.query("DELETE  FROM dba_lenguaje.user WHERE idUser =? ",[numero], function (err, result, fields) {
+    if (err) throw err;
+    else{
+        alert('usuario eliminado');
+      
+
+    }
+
+    });
+
+}
+
+function registrar(){
+   
+    var indicenuevo = contador + 1;
+    var usuario = document.getElementById("usernuevo").value;
+    var nombre = document.getElementById("nombrenuevo").value;
+    var contra = document.getElementById("passnuevo").value;
+    con.query("INSERT INTO dba_lenguaje.user  (idUser,user ,nombre,pass,tipo,calif ) VALUES(?,?,?,?,'Usuario',0) ",[indicenuevo,usuario,nombre,contra], function (err, result, fields) {
+    if (err) throw err;
+    else{
+        alert('usuario creado');
+     
+
+    }
+
+    });
+
+}
--- src/dict.js
+++ src/dict.js
@@ -1,9 +1,56 @@
 
 
+require('jquery');
+require('@popperjs/core');
+require('bootstrap');
+var mysql = require('mysql');
+var $ = require( "jquery" );
+require('bootstrap');
 // or the shorthand way
 const dict = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','V','W','Y','X','Z'];
 
 var longi = Object.keys(dict).length
 var numero = Math.floor(Math.random() * longi);
+
 document.getElementById("letra").innerHTML = dict[numero];
 console.log(dict[numero]);
+
+var letra = dict[numero];
+var valor = String(letra);
+var correcto = valor;
+function myFunction() {
+    document.getElementById("myImg").src = "src/img/"+ valor +'.JPG';
+  }
+myFunction();
+
+var boton =Math.floor(Math.random() * 4);
+console.log(boton);
+
+
+function llenar(){
+    var temp1 = Math.floor(Math.random() * longi);
+    var temp2 = Math.floor(Math.random() * longi);
+    if (boton == 0 || boton == 1){
+    
+    console.log(correcto);
+    document.getElementById('b1').value = correcto;
+    document.getElementById("b2").value = dict[temp1];
+    document.getElementById("b3").value = dict[temp2];
+
+    }else{
+        if (boton == 2 ){
+            document.getElementById('b1').value = dict[temp1];
+            document.getElementById("b2").value = correcto;
+            document.getElementById("b3").value = dict[temp2];
+    }else{
+        if (boton == 3 ){
+            document.getElementById('b1').value = dict[temp1];
+            document.getElementById("b2").value = dict[temp2];
+            document.getElementById("b3").value = correcto;}
+
+    }
+
+}
+}
+llenar();
+
